a18c4a64bf97f2323cfa14b18d727197bd00482e
Lusion2 <treysem20@gmail.com>
2024-05-13 17:34:16 -0400

    still working on variables in code generation

--- src/generate/generate.c
+++ src/generate/generate.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "generate.h"
 #include "../parse/parse.h"
@@ -9,12 +10,14 @@
 #include "../typedefs.h"
 #include "../alloc.h"
 
-size_t stack_size = 0;
-
 typedef struct{
     size_t stack_loc;
-    const char *ident;
+    char *ident;
 } stack_var;
+
+typedef dynlist(stack_var) dynlist_stack_var;
+size_t stack_size = 0;
+dynlist_stack_var stack_vars;
 
 
 void push(FILE *fptr, const char *reg){
@@ -22,20 +25,25 @@ void push(FILE *fptr, const char *reg){
     stack_size++;
 }
 
-void pop(FILE *fptr, const char *reg, size_t offset){
-    fprintf(fptr, "    lea rsp, [rsp + %i]\n", 8*offset);
+void pop(FILE *fptr, const char *reg){
     fprintf(fptr, "    pop %s\n", reg);
     stack_size--;
 }
 
-void pop(FILE *fptr, const char *reg, size_t offset){
-    fprintf(fptr, "    lea rsp, [rsp + %i]\n", 8*offset);
+void getVar(FILE *fptr, const char *reg, size_t offset){
+    fprintf(fptr, "    lea rsp, [rsp + %li]\n", 8*offset);
     fprintf(fptr, "    pop %s\n", reg);
     stack_size--;
 }
 
 void generate(dynlist_stmt *prog)
 {
+    // init the stack_var dynlist
+    dynlist_init(stack_vars, stack_var);
+    for(size_t i = 0; i < stack_vars.size; i++){
+        stack_vars.data[i].ident = (char*)malloc(sizeof(char));
+    }
+
     // Generate and open the file
     FILE *fptr;
     fptr = fopen("./build/out.S", "w");
@@ -45,7 +53,7 @@ void generate(dynlist_stmt *prog)
 
     for(u32 i = 0; i < prog->size; i++){
         node_stmt *n = prog->data[i];
-        printf("Statement #%i\n", i);
+        printf("Statement #%i\n", i+1);
         if(n->type == StmtType_exit){
             printf("\texit statement\n");
             if(n->expr->type == ExprType_Add){
@@ -54,7 +62,7 @@ void generate(dynlist_stmt *prog)
                 fprintf(fptr, "    add rax, rdi\n");
                 push(fptr, "rax");
                 fprintf(fptr, "    mov rax, 60\n");
-                pop(fptr, "rdi", 0);
+                pop(fptr, "rdi");
                 fprintf(fptr, "    syscall\n");
             }
             else if(n->expr->type == ExprType_Term){
@@ -64,7 +72,7 @@ void generate(dynlist_stmt *prog)
                 }
                 else if(n->expr->term->type == TermType_ident){
                     fprintf(fptr, "    mov rax, 60\n");
-                    pop(fptr, "rdi", 2);
+                    getVar(fptr, "rdi", 1);
                     fprintf(fptr, "    syscall\n");
                 }
             }
@@ -72,16 +80,47 @@ void generate(dynlist_stmt *prog)
         else if(n->type == StmtType_var){
             printf("\tvariable statement\n");
             if(n->expr->type == ExprType_Add){
+                // Creating a new variable
+                
+                // saving the variable in our stack struct
+                stack_var var = {.stack_loc = stack_size};
+                for(size_t i = 0; i < stack_vars.size; i++){
+                    if(strcmp(n->expr->term->ident.val, stack_vars.data[i].ident) == 0){
+                        fprintf(stderr, "ERROR Redefinition of variable: %s\n", n->expr->term->ident.val);
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+                // asm for this
                 fprintf(fptr, "    ; %s\n", n->expr->term->ident.val);
                 fprintf(fptr, "    mov rax, %s\n", n->expr->binary.left->term->int_lit.val);
                 fprintf(fptr, "    mov rdi, %s\n", n->expr->binary.right->term->int_lit.val);
                 fprintf(fptr, "    add rax, rdi\n");
                 push(fptr, "rax");
+
+                // this is done down here because it wasn't working when it was above
+                var.ident = n->expr->term->ident.val;
+                dynlist_push(stack_vars, var);
+            
             }
             else{
+                // saving the variable in our stack struct
+                stack_var var = {.stack_loc = stack_size};
+                for(size_t i = 0; i < stack_vars.size; i++){
+                    if(strcmp(n->expr->term->ident.val, stack_vars.data[i].ident) == 0){
+                        fprintf(stderr, "ERROR Redefinition of variable: %s\n", n->expr->term->ident.val);
+                        exit(EXIT_FAILURE);
+                    }
+                }  
+
                 fprintf(fptr, "    ; %s\n", n->expr->term->ident.val);
                 fprintf(fptr, "    mov rax, %s\n", n->expr->term->int_lit.val);
                 push(fptr, "rax");
+
+                // TODO : dynlist_push(stack_vars, var) NOT WORKING WTF
+                // this is done down here because it wasn't working when it was above
+                // var.ident = n->expr->term->ident.val;
+                // dynlist_push(stack_vars, var);
             }
         }
     }
@@ -91,4 +130,9 @@ void generate(dynlist_stmt *prog)
 
     // Close the file
     fclose(fptr);
+
+    for(size_t i = 0; i < stack_vars.size; i++){
+        free(stack_vars.data[i].ident);
+    }
+    dynlist_free(stack_vars);
 }
